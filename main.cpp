fd9c86417f5f7850125317397809837d20496fa8
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-06-15 10:23:57 +0300

    feat: ask for the minutes before shutting down

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 
 int main()
 {
+    int minutes_to_shutdown;
+    int seconds_to_shutdown;
+
+    cout << "How many minutes to shutdown the pc: ";
+    cin >> minutes_to_shutdown;
+
+    seconds_to_shutdown = minutes_to_shutdown * 60;
+    
+    for (int i = seconds_to_shutdown; i > 0; i--)
+    {
+        cout << "The pc will shutdown after " << i << " seconds" << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
 
     system("C:\\Windows\\System32\\shutdown /s");
     return 0;
